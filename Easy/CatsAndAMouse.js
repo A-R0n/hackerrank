13e43d1f4cb01f12a52db4ce27cfb97ded77c235
Aaron Estes <aestesc@gmail.com>
2019-04-21 21:48:31 -0500

    exclusive or..cat mouse

---
+++ Easy/CatsAndAMouse.js
@@ -0,0 +1,17 @@
+// Two cats and a mouse are at various positions on a line.
+// You will be given their starting positions. 
+// Your task is to determine which cat will reach the mouse first,
+// assuming the mouse doesn't move and the cats travel at equal speed. 
+// If the cats arrive at the same time,
+// the mouse will be allowed to move and it will escape while they fight.
+function catAndMouse(cat1, cat2, mouse) {
+    if (Math.abs(mouse-cat1) == Math.abs(mouse-cat2)) {
+        return 'Mouse C';
+    }
+    else if (Math.abs(cat2 - mouse) > Math.abs(cat1 - mouse)) {
+        return 'Cat A';
+    }
+    else {
+        return 'Cat B';
+    }
+}
---
+++ Easy/XORString.py
@@ -0,0 +1,10 @@
+# Exclusive or function returns true when
+# inputs differ, one is true the other false
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0';
+        else:
+            res += '1';
+    return res
