b3d5b633ece670f5d5156d4e0b99a81ec42aeca4
Aaron Estes <aestesc@gmail.com>
2019-04-25 23:25:42 -0500

    If char in string matches char in another string, return

---
+++ Easy/TwoStrings.py
@@ -0,0 +1,7 @@
+## Given two strings, determine if they have
+## a smaller substring.
+## Return 'YES' if substring is shared.
+## Return 'NO' if no substring exists.
+def twoStrings(s1, s2):
+    return 'YES' if any(i in s2 for i in s1 ) else 'NO'
+## only returns 'NO' if all iterations are false
