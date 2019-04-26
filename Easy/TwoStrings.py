2adb1abe4a7fd0f9fff68665a6a780ce2e79d386
Aaron Estes <aestesc@gmail.com>
2019-04-25 23:55:32 -0500

    Clarify purpose

--- Easy/TwoStrings.py
+++ Easy/TwoStrings.py
@@ -4,4 +4,4 @@
 ## Return 'NO' if no substring exists.
 def twoStrings(s1, s2):
     return 'YES' if any(i in s2 for i in s1 ) else 'NO'
-## only returns 'NO' if all iterations are false
+## only returns false if all values are false
